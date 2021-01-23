18500e95e6345d4fc7a99b5b2173c4b8d5aa0519
ManBytesDog <ThomasJ.Marstiller@yahoo.com>
2021-01-23 12:55:09 -0500

    passing

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,23 @@
+# class BirdsController < ApplicationController
+#   def index
+#     @birds = Bird.all
+#     render 'birds/index.html.erb'
+#   end
+# end
+
+# class BirdsController < ApplicationController
+#   def index
+#     @birds = Bird.all
+#     render plain: "Hello #{@birds[3].name}"
+#   end
+# end 
+
 class BirdsController < ApplicationController
   def index
     @birds = Bird.all
-    render 'birds/index.html.erb'
-  end
+    # render json: { message: 'Hashes of data will get converted to JSON' }
+    # render json: ['As','well','as','arrays']
+    # render json: @birds 
+    render json: { birds: @birds, messages: ['Hello Birds', 'Goodbye Birds'] }.to_json
+  end 
 end
